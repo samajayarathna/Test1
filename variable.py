b68d10a9ceee1473dff24c50a6a980d53420fc84
samajayarathna <55673667+samajayarathna@users.noreply.github.com>
2022-01-18 15:26:11 +1100

    update1

--- variable.py
+++ variable.py
@@ -1,4 +1,34 @@
-first_variable_name= 10
-second_variable_name= "Sama"
+first_variable_name = 10
+second_variable_name = "Sama"
 print(first_variable_name)
 print(second_variable_name)
+
+name = "Sama"
+print(type(name))
+
+number = 120
+print(type(number))
+
+x=2 
+y= str(x)
+print(y)
+
+var1= 63.245
+var2= int(var1)
+print(var2)
+
+var3 = "Australia"
+print(var3[2:6])
+
+var4 = "Strowberry blueberry banana mango "
+print(var4.split(". "))
+
+a = "Hello"
+b = "World"
+c= a + "$" + b
+print(c)
+print(a + "$" + b)
+
+var5 =  "I was walking \n \"in the park "
+print(var5)
+
